bd5a608bff4997cb395f86fa57684f4700ec4794
DESKTOP-B387RQR\JH <kjh21987@gmail.com>
2025-01-16 18:05:19 +0900

    Add src
    누락된 src 폴더 추가

---
+++ P02_SpringBoot/spring-practice/src/main/java/com/fastcampus/springpractice/Main.java
@@ -0,0 +1,14 @@
+package com.fastcampus.springpractice;
+
+import com.fastcampus.springpractice.logic.JavaSort;
+import com.fastcampus.springpractice.logic.Sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Sort<String> sort = new JavaSort<>();
+
+        System.out.println("[Result] " + sort.sort(Arrays.asList(args)));
+    }
+}
