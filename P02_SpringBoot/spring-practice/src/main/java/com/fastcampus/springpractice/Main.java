f59eac43f23bc424c68aceb6b7294005cb5139c5
DESKTOP-B387RQR\JH <kjh21987@gmail.com>
2025-01-18 00:02:32 +0900

    Config #5 - Spring Configuration
    기초 설정
    루트 패키지 이하에 빈이 있는지 스캔한다.

--- P02_SpringBoot/spring-practice/src/main/java/com/fastcampus/springpractice/Main.java
+++ P02_SpringBoot/spring-practice/src/main/java/com/fastcampus/springpractice/Main.java
@@ -1,14 +1,19 @@
 package com.fastcampus.springpractice;
 
+import com.fastcampus.springpractice.config.Config;
 import com.fastcampus.springpractice.logic.JavaSort;
 import com.fastcampus.springpractice.logic.Sort;
+import com.fastcampus.springpractice.service.SortService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Sort<String> sort = new JavaSort<>();
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        SortService sortService = context.getBean(SortService.class);
 
-        System.out.println("[Result] " + sort.sort(Arrays.asList(args)));
+        System.out.println("[Result] " + sortService.doSort(Arrays.asList(args)));
     }
 }
