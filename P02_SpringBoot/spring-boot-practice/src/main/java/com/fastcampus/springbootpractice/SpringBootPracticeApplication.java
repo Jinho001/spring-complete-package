4f0dfefea7429d9c5f19b105cbe62f48671c8961
kcloud <kjh21987@gmail.com>
2025-01-20 20:54:28 +0900

    tothehome
    집에가서 마저하자

--- P02_SpringBoot/spring-boot-practice/src/main/java/com/fastcampus/springbootpractice/SpringBootPracticeApplication.java
+++ P02_SpringBoot/spring-boot-practice/src/main/java/com/fastcampus/springbootpractice/SpringBootPracticeApplication.java
@@ -1,13 +1,29 @@
 package com.fastcampus.springbootpractice;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = WebMvcAutoConfiguration.class
+)
 public class SpringBootPracticeApplication {
+
+	@Value("${my.height}")
+	private int height;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPracticeApplication.class, args);
+
+		SpringBootPracticeApplication app = new SpringBootPracticeApplication();
+
+
 	}
 
+	@PostConstruct
+	public void init() {
+		System.out.println("[@Value] "+height);
+	}
 }
