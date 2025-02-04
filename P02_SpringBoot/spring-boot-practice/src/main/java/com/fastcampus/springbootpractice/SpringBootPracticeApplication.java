1b656dacd2dec3e00c70677f9985da1b7bee076a
kcloud <kjh21987@gmail.com>
2025-02-04 12:34:08 +0900

    250204
    데스크탑에서 작업을 위한 커밋

--- P02_SpringBoot/spring-boot-practice/src/main/java/com/fastcampus/springbootpractice/SpringBootPracticeApplication.java
+++ P02_SpringBoot/spring-boot-practice/src/main/java/com/fastcampus/springbootpractice/SpringBootPracticeApplication.java
@@ -1,29 +1,19 @@
 package com.fastcampus.springbootpractice;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication(
-		exclude = WebMvcAutoConfiguration.class
-)
+@SpringBootApplication
+@ConfigurationPropertiesScan
+@RequiredArgsConstructor
 public class SpringBootPracticeApplication {
 
-	@Value("${my.height}")
-	private int height;
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPracticeApplication.class, args);
-
-		SpringBootPracticeApplication app = new SpringBootPracticeApplication();
+    private final MyProperties myProperties;
 
 
-	}
-
-	@PostConstruct
-	public void init() {
-		System.out.println("[@Value] "+height);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPracticeApplication.class, args);
+    }
 }
